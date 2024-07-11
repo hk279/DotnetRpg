7522c4445595c7fc96467b34b0e6d9388de9a2e2
Jere Järvinen <83586982+visma-jere-jarvinen@users.noreply.github.com>
2024-07-11 11:37:15 +0300

    Add bootstrap logging

--- DotnetRpg/Program.cs
+++ DotnetRpg/Program.cs
@@ -14,97 +14,114 @@ using DotnetRpg;
 using DotnetRpg.Data.Seeding;
 using DotnetRpg.Services.InventoryService;
 using DotnetRpg.Services.UserProvider;
+using Serilog;
+using Serilog.Debugging;
 
-var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-var tokenSecret =
-    builder.Configuration.GetSection("TokenSettings:Secret").Value
-    ?? throw new ArgumentException("Missing token secret");
+SelfLog.Enable(Console.Error);
+Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateBootstrapLogger();
 
-// Add services to the container.
-builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(options =>
+try
 {
-    options.UseSqlServer(connectionString);
-    options.EnableSensitiveDataLogging();
-});
-builder.Services.AddHttpLogging(o => { });
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.AddSecurityDefinition(
-        "oauth2",
-        new OpenApiSecurityScheme
-        {
-            Description = "Standard authorization header using the Bearer scheme",
-            In = ParameterLocation.Header,
-            Name = "Authorization",
-            Type = SecuritySchemeType.ApiKey
-        }
-    );
-    c.OperationFilter<SecurityRequirementsOperationFilter>();
-});
-builder.Services.AddAutoMapper(typeof(Program).Assembly);
-builder.Services
-    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
+    var builder = WebApplication.CreateBuilder(args);
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    var tokenSecret =
+        builder.Configuration.GetSection("TokenSettings:Secret").Value
+        ?? throw new ArgumentException("Missing token secret");
+
+    // Add services to the container.
+    builder.Services.AddControllers();
+    builder.Services.AddDbContext<DataContext>(options =>
     {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenSecret)),
-            ValidateIssuer = false,
-            ValidateAudience = false
-        };
+        options.UseSqlServer(connectionString);
+        options.EnableSensitiveDataLogging();
     });
-builder.Services.AddHttpContextAccessor();
+    builder.Services.AddHttpLogging(o => { });
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(c =>
+    {
+        c.AddSecurityDefinition(
+            "oauth2",
+            new OpenApiSecurityScheme
+            {
+                Description = "Standard authorization header using the Bearer scheme",
+                In = ParameterLocation.Header,
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey
+            }
+        );
+        c.OperationFilter<SecurityRequirementsOperationFilter>();
+    });
+    builder.Services.AddAutoMapper(typeof(Program).Assembly);
+    builder.Services
+        .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenSecret)),
+                ValidateIssuer = false,
+                ValidateAudience = false
+            };
+        });
+    builder.Services.AddHttpContextAccessor();
 
-builder.Services.AddScoped<ICharacterService, CharacterService>();
-builder.Services.AddScoped<IAuthService, AuthService>();
-builder.Services.AddScoped<IFightService, FightService>();
-builder.Services.AddScoped<IEnemyGeneratorService, EnemyGeneratorService>();
-builder.Services.AddScoped<IInventoryService, InventoryService>();
+    builder.Services.AddScoped<ICharacterService, CharacterService>();
+    builder.Services.AddScoped<IAuthService, AuthService>();
+    builder.Services.AddScoped<IFightService, FightService>();
+    builder.Services.AddScoped<IEnemyGeneratorService, EnemyGeneratorService>();
+    builder.Services.AddScoped<IInventoryService, InventoryService>();
 
-builder.Services.AddScoped<IUserProvider, AuthenticatedUserProvider>();
+    builder.Services.AddScoped<IUserProvider, AuthenticatedUserProvider>();
 
-builder.Services.AddTransient<GlobalExceptionHandlerMiddleware>();
+    builder.Services.AddTransient<GlobalExceptionHandlerMiddleware>();
 
-var app = builder.Build();
+    var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
 
-    app.UseCors(
-        corsPolicyBuilder =>
-            corsPolicyBuilder
-                .WithOrigins("http://localhost:3000")
-                .AllowAnyHeader()
-                .AllowAnyMethod()
-                .AllowCredentials()
-    );
+        app.UseCors(
+            corsPolicyBuilder =>
+                corsPolicyBuilder
+                    .WithOrigins("http://localhost:3000")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowCredentials()
+        );
+    }
+
+    app.UseHttpLogging();
+
+    app.UseMiddleware<GlobalExceptionHandlerMiddleware>();
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthentication();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
+    optionsBuilder.UseSqlServer(connectionString);
+    optionsBuilder.EnableSensitiveDataLogging();
+
+    var context = new DataContext(optionsBuilder.Options, new MockUserProvider());
+
+    await DataSeeder.SeedData(context);
+
+    app.Run();
 }
-
-app.UseHttpLogging();
-
-app.UseMiddleware<GlobalExceptionHandlerMiddleware>();
-
-app.UseHttpsRedirection();
-
-app.UseAuthentication();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
-optionsBuilder.UseSqlServer(connectionString);
-optionsBuilder.EnableSensitiveDataLogging();
-
-var context = new DataContext(optionsBuilder.Options, new MockUserProvider());
-
-await DataSeeder.SeedData(context);
-
-app.Run();
+catch (Exception e)
+{
+    Log.Fatal(e, "Host terminated unexpectedly");
+    throw;
+}
+finally
+{
+    Log.CloseAndFlush();
+}
